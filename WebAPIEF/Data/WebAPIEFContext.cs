b3149d6c90dd21346e4ea8147d1c37f5689a7232
BrianMarquez3 <brian3marquez@gmail.com>
2021-08-28 22:18:02 -0500

    Update

---
+++ WebAPIEF/Data/WebAPIEFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIEF.Models;
+
+namespace WebAPIEF.Data
+{
+    public class WebAPIEFContext : DbContext
+    {
+        public WebAPIEFContext (DbContextOptions<WebAPIEFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIEF.Models.Book> Book { get; set; }
+    }
+}
---
+++ WebAPIEF/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIEF.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+
+    }
+}
